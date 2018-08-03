32b1575d2bc8113fbf5e7d8a7da20f3e12a094bb
Stefan Mutter <stefan.mutter@sahmri.com>
2018-08-03 14:14:43 +0300

    add adjustment of phenotype scores with linear regression. The adjusted scores are the residuals of the linear regression with the unadjusted score as the response variable (y) and the confounders as dependent variables (x). No intercept is used

--- R/responseScore.R
+++ R/responseScore.R
@@ -5,8 +5,9 @@
 #' @param responseDf the data frame that contains the time blocks and the biomarker data per time block
 #' @param idColumn name of ID column
 #' @param blockColumn name of time block column
-#' @param sexColumn name of column that contains sex information (optional)
+#' @param sexColumn name of column that contains sex information (optional). If provided, the score will be adjusted for sex.
 #' @param responseColumn names of the columns containing the patient block value for a biomarker (e.g. vector of string names). The score will be based only on these columns.
+#' @param confounderColumns names of columns that contain confounders (optional). If provided, the score will be adjusted for these confounders using linear regression
 #' @return the response data frame with an added column phenoScore that shows the computed score
 #' @export
 #' @importFrom magrittr %>%
@@ -17,9 +18,9 @@
 #'
 #' resultsCor <- calculateResponseScore(results,responseColumns = c('sbp','dbp','aerAll','acrAll','egfr'), sexColumn = 'SEX')
 #' }
-calculateResponseScore <- function(responseDf, idColumn = 'PATIENT', blockColumn = 'block', sexColumn = NA, responseColumns){
-  if(length(responseColumns) < 1 ){
-    stop('No response columns provided')
+calculateResponseScore <- function(responseDf, idColumn = 'PATIENT', blockColumn = 'block', sexColumn = NA, responseColumns, confounderColumns = NA){
+  if(length(responseColumns) < 2 ){
+    stop('Provide at least 2 response columns')
   }
   responseDf <- checkResponseFormat(responseDf,idColumn = idColumn, blockColumn = blockColumn, sexColumn = sexColumn, responseColumns = responseColumns)
   if(is.null(responseDf)){
@@ -46,7 +47,7 @@ calculateResponseScore <- function(responseDf, idColumn = 'PATIENT', blockColumn
    responseDf <- rbind(responseDf1,responseDf2) %>% as.data.frame()
    responseDf <- responseDf[order(responseDf$PATIENT, responseDf$BLOCK),]
   }
-  
+  responseDf <- responseDf %>% adjustScore(confounderColumns = confounderColumns, responseColumns = responseColumns)
   responseDf
 }
 
@@ -128,6 +129,23 @@ calculateScore <- function(responseDf, responseColumns, firstPrincipalComponent)
   responseNoMissingAll <- responseNoMissingAll %>% select(PATIENT,BLOCK,phenoScore = `blockData$phenoScore`)
   responseDf <- merge(responseDf,responseNoMissingAll, by = c('PATIENT','BLOCK'), all = TRUE)
   responseDf  
+}
+
+#' Get the score from the first PC
+#'
+#' @param responseDf the data frame that contains the time blocks and the scores and the confounders per time block
+#' @param scoreColumn name of the column that contains the score
+#' @param confounderColumns names of the columns that contain confounding information
+#' @param responseColumn names of the columns containing the patient block value for a biomarker (e.g. vector of string names). The score will be based only on these columns.
+#' @return the response data frame with an adjusted score
+#' @keywords internal
+adjustScore <- function(responseDf, scoreColumn = 'phenoScore', confounderColumns, responseColumns){
+  if(!any(is.na(confounderColumns))){
+    myFormula <- reformulate(termlabels = c(0,confounderColumns), response = scoreColumn)
+    fit <- lm(myFormula, data=responseDf, na.action = na.exclude)
+    responseDf[,scoreColumn] <- ifelse(is.na(residuals(fit)),responseDf[,scoreColumn],residuals(fit))
+  }
+  responseDf
 }
 
 
